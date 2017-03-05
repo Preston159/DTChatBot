8a43339a692bacec4823c3a75eb91f3cf9a052a7
Preston <prestonpetrie@gmail.com>
2017-03-04 20:17:28 -0600

    Added support for requiring certain role to change settings

--- src/main/java/com/preston159/dtbot/Main.java
+++ src/main/java/com/preston159/dtbot/Main.java
@@ -11,7 +11,9 @@ import com.google.common.util.concurrent.FutureCallback;
 import de.btobastian.javacord.DiscordAPI;
 import de.btobastian.javacord.Javacord;
 import de.btobastian.javacord.entities.Channel;
+import de.btobastian.javacord.entities.Server;
 import de.btobastian.javacord.entities.message.Message;
+import de.btobastian.javacord.entities.permissions.Role;
 import de.btobastian.javacord.listener.message.MessageCreateListener;
 
 public class Main {
@@ -25,6 +27,7 @@ public class Main {
 	public static final int tChannel = 1;
 	public static final int dChannel = 2;
 	public static final int ircBot = 3;
+	public static final int reqRole = 4;
 	
 	public static HashMap<String, Object[]> servers = new HashMap<String, Object[]>();
 	
@@ -40,43 +43,78 @@ public class Main {
 						String[] messageA = message.getContent().toLowerCase().split(" ");
 						if(messageA.length == 0)
 							return;
-						if(messageA[0].equals("!setchannel")) {
+						boolean hasRole = false;
+						final Channel channel = message.getChannelReceiver();
+						Server server = channel.getServer();
+						if(!servers.containsKey(server.getId())) {
+							hasRole = true;
+						} else if(servers.get(server.getId())[reqRole] != null) {
+							String roleID = (String) servers.get(server.getId())[reqRole];
+							for(Role r : message.getAuthor().getRoles(server)) {
+								if(r.getId().equalsIgnoreCase(roleID))
+									hasRole = true;
+							}
+						} else {
+							hasRole = true;
+						}
+						if(messageA[0].equals("!setchannel")) BLOCK: {
+							if(!hasRole) {
+								channel.sendMessage("You don't have permission to do that, " + message.getAuthor().getName());
+								break BLOCK;
+							}
 							if(messageA.length == 1) {
-								Channel channel = message.getChannelReceiver();
 								message.getChannelReceiver().sendMessage("Discord channel set to #" + channel.toString());
 								if(!servers.containsKey(message.getChannelReceiver().getServer().getId())) {
 									addServer(message.getChannelReceiver().getServer().getId(), null, channel);
 								}
 								servers.get(message.getChannelReceiver().getServer().getId())[dChannel] = channel;
 							} else {
-								String channel = "#" + messageA[1].toLowerCase();
+								String twitchChannel = "#" + messageA[1].toLowerCase();
 								if(!servers.containsKey(message.getChannelReceiver().getServer().getId())) {
-									addServer(message.getChannelReceiver().getServer().getId(), channel, null);
+									addServer(message.getChannelReceiver().getServer().getId(), twitchChannel, null);
 								}
 								String serverID = message.getChannelReceiver().getServer().getId();
 								String oldChannel = (String) servers.get(serverID)[tChannel];
-								servers.get(serverID)[tChannel] = channel;
-								((IrcBot) servers.get(serverID)[ircBot]).switchChannel(oldChannel, channel);
+								servers.get(serverID)[tChannel] = twitchChannel;
+								((IrcBot) servers.get(serverID)[ircBot]).switchChannel(oldChannel, twitchChannel);
 								if(oldChannel != null) {
-									message.getChannelReceiver().sendMessage("Twitch channel changed from " + oldChannel.substring(1)
-											+ " to " + channel.substring(1));
+									message.getChannelReceiver().sendMessage("Twitch twitchChannel changed from " + oldChannel.substring(1)
+											+ " to " + twitchChannel.substring(1));
 								} else {
-									message.getChannelReceiver().sendMessage("Twitch channel set to " + channel.substring(1));
+									message.getChannelReceiver().sendMessage("Twitch twitchChannel set to " + twitchChannel.substring(1));
 								}
 							}
 						} else if(messageA[0].equals("!about")) {
-							final Channel channel = message.getChannelReceiver();
 							Runnable task = () -> {
 								channel.sendMessage("I am a bot which relays Twitch chat to a Discord channel\n" +
 										"I am currently in beta, so please be nice\n" +
 										"I currently don't save data, so if I'm restarted or crash you will have to reset me\n" +
-										"There's currently no way to restrict my usage, so anyone on your server can change my settings\n" +
-										"To set the Discord channel you want me to relay chat to, use `!setchannel`\n" +
+										"To set the required role to change my settings, use `!setrole <role name>`\n" +
+										"Currently, you can set the required role to one you aren't in, so be careful!\n" +
+										"To set the Discord channel you want me to relay chat to, use `!setchannel` in that channel\n" +
 										"To set the Twitch chat you want me to relay from, use `!setchannel <username>`");
 
 							};
 							Thread thread = new Thread(task);
 							thread.start();
+						} else if(messageA[0].equals("!setrole")) BLOCK: {
+							if(!hasRole) {
+								channel.sendMessage("You don't have permission to do that, " + message.getAuthor().getName());
+								break BLOCK;
+							}
+							if(messageA.length < 2) {
+								servers.get(server.getId())[reqRole] = null;
+								channel.sendMessage("Role requirement removed");
+								break BLOCK;
+							}
+							String newRole = messageA[1];
+							for(Role r : server.getRoles()) {
+								if(r.getName().equalsIgnoreCase(newRole)) {
+									servers.get(server.getId())[reqRole] = r.getId();
+									channel.sendMessage("Required role changed to " + r.getName());
+									break;
+								}
+							}
 						}
 					/*	else if(!message.getAuthor().getName().equalsIgnoreCase(DISCORD_USERNAME) &&
 								message.getContent().substring(0, 1) != "!") {
@@ -111,11 +149,12 @@ public class Main {
 	}
 	
 	private static void addServer(Object... server) {
-		Object[] record = new Object[4];
+		Object[] record = new Object[5];
 		record[0] = server[0];
 		record[1] = server[1];
 		record[2] = server[2];
 		record[3] = server.length == 3 ? createBot((String) record[0]) : server[3];
+		record[4] = null;
 		servers.put((String) record[0], record);
 	}
 }
