e3adf611452ae5ee02cd54c21739a78db2e42ba6
Preston <prestonpetrie@gmail.com>
2017-03-11 14:53:06 -0600

    Made Twitch chat names bold in Discord

--- src/main/java/com/preston159/dtbot/IrcBot.java
+++ src/main/java/com/preston159/dtbot/IrcBot.java
@@ -42,7 +42,7 @@ public class IrcBot extends PircBot {
 	
 	public void mReceive(String channel, String sender, String message) {
 		if(!sender.equalsIgnoreCase(Main.TWITCH_USERNAME))
-			Main.sendMessage(((Channel) Main.servers.get(sID)[Main.dChannel]), sender + ": " + Main.escape(message));
+			Main.sendMessage(((Channel) Main.servers.get(sID)[Main.dChannel]), "**" + Main.escape(sender) + "**: " + Main.escape(message));
 	}
 	
 	public void onPrivateMessage(String sender, String login, String hostname, String message) {
